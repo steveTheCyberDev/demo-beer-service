205498c5d68f0a708495751cf50a2d8a4a3c83c4
Steve Chen <steve.thecyberdev@gmail.com>
2021-07-31 23:36:29 +1000

    init demo commit

---
+++ src/main/java/guru/springframework/msscbrewery/services/BeerService.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+
+import java.util.UUID;
+
+public interface BeerService {
+    BeerDto getBeerById(UUID beerId);
+
+    BeerDto saveNewBeer(BeerDto beerDto);
+
+    BeerDto updateBeer(UUID beerId, BeerDto beerDto);
+
+    void deleteById(UUID beerId);
+}
---
+++ src/main/java/guru/springframework/msscbrewery/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package guru.springframework.msscbrewery.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, SYOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
